4c2c1ac1d17a6029766df6739e2040777e889fed
Szabolcs Toth <tsz@purzelbaum.hu>
2023-04-01 13:03:32 +0200

    Fixed: naming convention fixed to follow Oracle documentation.

--- IAM/list_users.rb
+++ IAM/list_users.rb
@@ -13,7 +13,7 @@
 
 require 'oci'
 
-api = OCI::Identity::IdentityClient.new(region: OCI::Regions::REGION_EU_FRANKFURT_1)
+identity_client = OCI::Identity::IdentityClient.new(region: OCI::Regions::REGION_EU_FRANKFURT_1)
 # get tenancy id from your config file
-response = api.list_users(OCI.config.tenancy)
+response = identity_client.list_users(OCI.config.tenancy)
 response.data.each { |user| puts user.name }
