f8f8416ad14cee7784c7315e061e2eb5dca26628
Szabolcs Toth <tsz@purzelbaum.hu>
2023-04-01 12:40:04 +0200

    Added: IAM folder with README.md.

---
+++ IAM/list_users.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+# This script demonstrates how to list all the users in your tenancy. 
+# Created by Szabolcs Toth, 01-04-2023
+#
+# This will use credentials and settings from the DEFAULT profile at ~/.oci/config (on Windows "C:\Users\{user}\.oci\config").
+#
+# Format:
+#   list.user.rb
+#
+# Example run:
+#   ruby list.user.rb
+
+require 'oci'
+
+api = OCI::Identity::IdentityClient.new(region: OCI::Regions::REGION_EU_FRANKFURT_1)
+# get tenancy name from your config file
+response = api.list_users(OCI.config.tenancy)
+response.data.each { |user| puts user.name }
